8d5dfdb55d608915531571e0db1b94da6f1b7b4e
Kelly <long.redhair.tmge.106@gmail.com>
2023-04-25 14:18:50 +0900

    不要ファイル削除

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,16 +1,53 @@
 import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App.vue', () => {
-  const wrapper = mount(App)
   it('App スナップショット', () => {
     expect(wrapper).toMatchSnapshot()
   })
   it('コンポーネントを描画できる', () => {
     // Componentを描画できる
-    const wrapper = mount(App)
+    const wrapper = shallowMount(App)
     const comp = wrapper.findComponent(App)
     expect(comp.exists()).toBe(true)
+  })
+
+  it('ゲームの状態を正しく初期化する', () => {
+    const wrapper = shallowMount(App)
+    expect(wrapper.vm.states).toEqual([
+      [-1, -1, -1],
+      [-1, -1, -1],
+      [-1, -1, -1]
+    ])
+  })
+
+  it('選択後、playerIdを更新する', () => {
+    const wrapper = shallowMount(App)
+    const selectCell = wrapper.vm.onSelect(0, 0)
+    console.debug('selectCell: ', selectCell)
+    expect(wrapper.vm.playerId).toEqual(2)
+  })
+
+  it('勝敗を判定する', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.states = [
+      [1, 1, 2],
+      [-1, 2, 1],
+      [2, -1, 2]
+    ]
+    const winnerId = wrapper.vm.getWinnerId()
+    expect(winnerId).toEqual(2)
+  })
+
+  it('ドローを検出する', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.states = [
+      [1, 2, 1],
+      [1, 2, 2],
+      [2, 1, 2]
+    ]
+    const isDraw = wrapper.vm.isDraw()
+    expect(isDraw).toEqual(true)
   })
 })
